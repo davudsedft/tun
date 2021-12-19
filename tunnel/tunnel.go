xxlive/clash

enlarge buffer size of {tcp,udp}Queue to 65536
Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

tunnel/tunnel.go
@@ -16,8 +16,8 @@ import (
 )
 
 var (
-	tcpQueue  = make(chan C.ServerAdapter, 200)
-	udpQueue  = make(chan *inbound.PacketAdapter, 200)
+	tcpQueue  = make(chan C.ServerAdapter, 65536)
+	udpQueue  = make(chan *inbound.PacketAdapter, 65536)
 	natTable  = nat.New()
 	rules     []C.Rule
 	proxies   = make(map[string]C.Proxy)

davudsedft/tun

tunnel/tunnel.go
@@ -19,8 +19,8 @@ import (
 )
 
 var (
-	tcpQueue  = make(chan C.ConnContext, 200)
-	udpQueue  = make(chan *inbound.PacketAdapter, 200)
+	tcpQueue  = make(chan C.ConnContext, 65536)
+	udpQueue  = make(chan *inbound.PacketAdapter, 65536)
 	natTable  = nat.New()
 	rules     []C.Rule
 	proxies   = make(map[string]C.Proxy)